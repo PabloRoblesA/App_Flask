b29ab30086b0ca032e156f14e8526b9159b2d6ea
Pablo Robles <156269638+PabloRoblesA@users.noreply.github.com>
2024-07-15 22:00:14 +0000

    #FaseRender3

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 #pip install flask
 from app import app
+from app import routes
 from flask import Flask, request, render_template
 from pickle import load
 import pandas as pd
 
-app = Flask(__name__)
+app1 = Flask(__name__)
 model = load(open("decision_tree_classifier_default_42.sav", "rb"))
 
 class_dict = {
@@ -13,7 +14,7 @@ class_dict = {
     "2": "Iris virginica"
 }
 
-@app.route("/", methods = ["GET", "POST"])
+@app1.route("/", methods = ["GET", "POST"])
 def index():
     if request.method == "POST":        
         val1 = float(request.form["val1"])
